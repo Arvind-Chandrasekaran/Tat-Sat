493e6b271d3cbb7be96c04cb12c5d58dcd10ef4c
Arvind Chandrasekaran <126087234+Arvind-Chandrasekaran@users.noreply.github.com>
2026-09-01 11:30:25 +1000

    server:app

--- post_creation/server.py
+++ post_creation/server.py
@@ -7,5 +7,13 @@ app = FastAPI(
         docs_url="/api-reference")
 app.include_router(router)
 
-uvicorn.run(app, host="127.0.0.1", port=8000)
+
+"""
+if __name__ == "__main__":
+        uvicorn.run(app, host="127.0.0.1", port=8000)
+
+        # 127.0.0.1 is a local ip address. For production change it 0.0.0.0 which will make it access any address in the production environment.
+
+Better to use uvicorn run -  uvicorn server:app --host 127.0.0.1 --port 8000      
+"""
 
